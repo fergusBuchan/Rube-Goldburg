ca23c44406a62de5bd479a43216b184076fa44cd
1702707 <rnaeri523@gmail.com.com>
2021-03-15 17:14:33 +0000

    Build 3 Complete

--- Source/Goldburg/GoldburgGameModeBase.cpp
+++ Source/Goldburg/GoldburgGameModeBase.cpp
@@ -14,15 +14,8 @@ void AGoldburgGameModeBase::BeginPlay()
 	onScreen->AddToViewport();
 	if (PlayerHUDClass != nullptr)
 	{
-<<<<<<< HEAD
-		if (onScreen != nullptr)
-		{	
-=======
-		
 		if (onScreen != nullptr)
 		{
-			
->>>>>>> parent of d53b90c (menu added)
 		}
 	}
 }
